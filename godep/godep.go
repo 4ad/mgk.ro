5e385ec07ddcdbce56fa8deb78496f4781f7945b
Aram Hăvărneanu <aram@mgk.ro>
2012-03-23 18:33:46 +0100

    godep: new tool.

---
+++ godep/godep.go
@@ -0,0 +1,54 @@
+// Permission to use, copy, modify, and/or distribute this software for
+// any purpose is hereby granted, provided this notice appear in all copies.
+
+// godep returns package dependency information.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	dot  = flag.Bool("dot", false, "print DOT language (GraphWiz)")
+	png  = flag.String("png", "", "write graph to png file")
+	tags = flag.String("tags", "", "additional built tags to consider")
+)
+
+var usageString = `usage: godep [options] [packages]
+
+Godep prints dependency information for packages named by the import
+paths. If no packages are specified, the current directory is assumed
+to be the package. The special import path "all" expands to all package
+directories found in all the GOPATH trees. The special import path "std"
+is like all but expands to just the packages in the standard Go library.
+
+For more about specifying packages, see 'go help packages'.
+
+Options:`
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "%s\n", usageString)
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func logf(format string, args ...interface{}) {
+	fmt.Fprint(os.Stderr, "godep: ")
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+}
+
+func log(args ...interface{}) { logf("%v", args...) }
+
+func fatalf(format string, args ...interface{}) {
+	logf(format, args...)
+	os.Exit(2)
+}
+
+func fatal(args ...interface{}) { fatalf("%v", args...) }
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+}
