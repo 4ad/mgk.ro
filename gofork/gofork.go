4c225bc8236e6a050d2aaa6b906da04e639f0e9c
Aram Hăvărneanu <aram@mgk.ro>
2012-03-29 22:11:06 +0200

    gofork: added documentation.

--- gofork/gofork.go
+++ gofork/gofork.go
@@ -1,6 +1,34 @@
 // Permission to use, copy, modify, and/or distribute this software for
 // any purpose is hereby granted, provided this notice appear in all copies.
 
+/*
+Gofork is a tool to help manage forked packages.
+
+Usage:
+    gofork old new
+    gofork -r 'old=new' [packages ...]
+
+In its first invocation gofork creates a fork of the package tree
+rooted at the old import path. Packages that wish to use the fork
+should use the new import path. gofork potentially updates the
+import paths of the packages in the tree in order to reference
+the forked packages. Forked packages will be installed where go
+get would install packages.
+
+In its second form gofork will update the named packages to
+change old import paths to the new fork. It works recursively,
+if you forked:
+
+    gofork net/http myhttp
+
+and you wish to use myhttp and myhttp/httputil in godoc, it
+suffices to:
+
+    gofork -r 'net/http=myhttp' cmd/godoc
+
+net/http will be changed to myhttp and net/http/httputil will be
+changed to myhttp/httputil.
+*/
 package main
 
 import (
@@ -12,10 +40,20 @@ import (
 )
 
 var (
-	flagR = flag.String("r", "", "")
+	flagR = flag.String("r", "", "update named packages to use the new import path")
 )
 
+var usageString = `usage:
+    gofork old new
+    gofork -r 'old=new' [packages ...]
+
+Options:
+`
+
+
 func usage() {
+	fmt.Fprint(os.Stderr, usageString)
+	flag.PrintDefaults()
 	os.Exit(1)
 }
 
@@ -28,7 +66,7 @@ func main() {
 }
 
 // srcDir returns the directory where the package with the named
-// import path resides. It is required for resolving local imports (ugh).
+// import path resides.
 func srcDir(path string) string {
 	// Check if it's a command in $GOROOT/src, like cmd/go.
 	cmdpath := filepath.Join(build.Default.GOROOT, "src", path)
@@ -46,7 +84,7 @@ func srcDir(path string) string {
 }
 
 func logf(format string, args ...interface{}) {
-	fmt.Fprint(os.Stderr, "godep: ")
+	fmt.Fprint(os.Stderr, "gofork: ")
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
 }
 
