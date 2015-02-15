9fec866116033fb3e9b604ece67d456d67c8abfa
Aram Hăvărneanu <aram@mgk.ro>
2015-02-15 14:32:23 +0100

    gometad: serve files from /var/www

--- cmd/gometad/main.go
+++ cmd/gometad/main.go
@@ -2,6 +2,8 @@
 Gometad: go get vanity redirector
 
 Gometad serves HTML meta tags over HTTP in order to implement mgk.ro vanity domain redirect to https://github.com/4ad/mgk.ro.git.
+
+It also serves files in /var/www, because why not.
 */
 package main
 
@@ -14,6 +16,7 @@ import (
 var meta = `<meta name="go-import" content="mgk.ro git https://github.com/4ad/mgk.ro.git">`
 
 func main() {
+	http.Handle("/pub/", http.StripPrefix("/pub/", http.FileServer(http.Dir("/var/www"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, meta)
 	})
