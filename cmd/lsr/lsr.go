49ad9d576e165c5b6be61ff8570af2733ec6929e
Aram Hăvărneanu <aram@mgk.ro>
2018-11-19 13:18:33 +0100

    lsr: ignore hidden files by default

--- cmd/lsr/lsr.go
+++ cmd/lsr/lsr.go
@@ -14,17 +14,18 @@
 
 /*
 Lsr: list recursively.
-	lsr [-d | -fd] [-l] [-n] [name ...]
+	lsr [-d | -fd] [-l] [-a] [name ...]
 
-For each directory argument, lsr recursively lists the contents of the
-directory; for each file argument, lsr repeats its name. When no argument
-is given, the current directory is listed.
+For each directory argument, lsr recursively lists the contents of
+the directory; for each file argument, lsr repeats its name. When
+no argument is given, the current directory is listed. Hidden files
+are ignored by default.
 
 Options:
     -d  only print directories
     -fd print both files and directories
     -l  list in long format; name mode mtime size
-    -n don't print hidden files or directories
+    -a print hidden files or directories
 */
 package main
 
@@ -42,10 +43,10 @@ var (
 	flagD  = flag.Bool("d", false, "only print directories")
 	flagFD = flag.Bool("fd", false, "print both files and directories")
 	flagL  = flag.Bool("l", false, "long format")
-	flagN  = flag.Bool("n", false, "don't print hidden files")
+	flagA  = flag.Bool("a", false, "print hidden files")
 )
 
-var usageString = `usage: lsr [-d | -fd] [-l] [-n] [name ...]
+var usageString = `usage: lsr [-d | -fd] [-l] [-a] [name ...]
 Options:
 `
 
@@ -59,7 +60,7 @@ func prname(path string, f os.FileInfo) error {
 	if f.IsDir() && path[len(path)-1] != '/' {
 		path = path + "/"
 	}
-	if *flagN {
+	if !*flagA {
 		ok, err := filepath.Match(".?*", filepath.Base(path))
 		if ok {
 			return nil
@@ -84,7 +85,7 @@ func pr(path string, f os.FileInfo, err error) error {
 		log.Println(err)
 		return nil
 	}
-	if *flagN && f.IsDir() {
+	if !*flagA && f.IsDir() {
 		ok, err := filepath.Match(".?*", filepath.Base(path))
 		if ok {
 			return filepath.SkipDir
